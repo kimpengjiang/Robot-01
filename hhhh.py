5d090d6cee43c544c99cb89acb04b5355457e5b4
BlackDog-HaHaHa <3089476870@qq.com>
2019-09-16 17:35:55 +0800

    robot-01

--- hhhh.py
+++
@@ -1,4 +0,0 @@
-import requests, re
-header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'}
-response = requests.get('https://www.dmmsee.icu/SSNI-577',headers=header)
-data = re.findall('tr*',response.text)
